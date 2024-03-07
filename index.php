39c1a7a57e46fc3c5192b5ce00c19a48a6707164
GiacomoMilitello <g.militello95@gmail.com>
2024-03-07 19:16:24 +0100

    starting

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+    class Movie{
+        public $title;
+        public $director;
+        public $releaseYear;
+        public $genre;
+    }
+
+    
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>php-oop-1</title>
+</head>
+<body>
+    
+</body>
+</html>
